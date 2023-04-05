0ddbd9cff73fee3eb87619e1c4ea3495f72bfe7c
Malrus-dev <maelsobfer@gmail.com>
2023-04-05 18:07:51 -0300

    refactor: refatora endpoint sale

--- back-end/src/service/sale.service.js
+++ back-end/src/service/sale.service.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const { Products, Sales, SalesProducts, User } = require('../database/models');
+const { Product, Sales, SalesProducts, User } = require('../database/models');
 const config = require('../database/config/config');
 const statusCode = require('../utils/statusCode');
 const { processError } = require('../utils/handleError');
@@ -8,20 +8,29 @@ const env = process.env.NODE_ENV || 'development';
 
 const sequelize = new Sequelize(config[env]);
 
+const continueRegister = async (products, t) => {
+  await SalesProducts.bulkCreate(products, { transaction: t });
+};
+
+const newSaleObj = (newSale) => {
+  const { userId, sellerId, deliveryAddress, deliveryNumber, totalPrice } = newSale;
+  return { userId,
+    sellerId,
+    deliveryAddress,
+    deliveryNumber,
+    status: 'Pendente', 
+    totalPrice,
+  };
+};
+
 const register = async (newSale) => {
   const t = await sequelize.transaction();
-  try {
-    const { userId, sellerId, deliveryAddress, deliveryNumber, products, totalPrice } = newSale;
-    const sale = await Sales.create(
-      { userId, sellerId, deliveryAddress, deliveryNumber, status: 'Pendente', totalPrice }, 
-      { transaction: t },
-      );
-      
-    await SalesProducts.bulkCreate(
-      products.map(({ id, quantity }) => ({ quantity, productId: id, saleId: sale.id })), 
-     { transaction: t },
-);
+  try {    
+    const sale = await Sales.create(newSaleObj(newSale), { transaction: t });
+    const newProducts = newSale.products
+      .map(({ id, quantity }) => ({ quantity, productId: id, saleId: sale.id }));  
 
+    await continueRegister(newProducts, t);
     await t.commit();
 
     return { message: sale };
@@ -47,7 +56,7 @@ const getDetails = async (saleId) => {
     const saleDetails = await Sales.findByPk(saleId, {
       include: [
         { model: User, as: 'seller', attributes: ['id', 'name'] }, 
-        { model: Products, as: 'products', through: { attributes: ['quantity'] } },
+        { model: Product, as: 'products', through: { attributes: ['quantity'] } },
       ],
     });
   
